98466aaa60077dce162a37fbb02912a6ea049e7f
José Pablo Sánchez Saucedo <pablo.sanchezs@uanl.edu.mx>
2024-11-16 22:44:07 -0600

    Se creo el archivo de VS, junto con las migraciones y las CRUDS de paquetes y opiniones

---
+++ PIA/PIA/Models/dbModels/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace PIA.Models.dbModels
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+
+        [StringLength(50)]
+        [Unicode(false)]
+        public required string Nombre { get; set; }
+
+        [InverseProperty("IdUsuarioNavigation")]
+        public virtual ICollection<Opinione> Opiniones { get; set; } = new List<Opinione>();
+
+        [InverseProperty("IdUsuarioNavigation")]
+        public virtual ICollection<Venta> Venta { get; set; } = new List<Venta>();
+    }
+}
