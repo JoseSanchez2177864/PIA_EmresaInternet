5d1e4870fb6a24de12b1779ea65590f41e3689ae
José Pablo Sánchez Saucedo <pablo.sanchezs@uanl.edu.mx>
2024-11-17 00:59:32 -0600

    Se solucionó el problema con la base de datos

---
+++ VS/PIA_PWEB/PIA_PWEB/Models/dbModels/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace PIA_PWEB.Models.dbModels
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+
+        [InverseProperty("IdUsuarioNavigation")]
+        public virtual ICollection<Opinione> Opiniones { get; set; } = new List<Opinione>();
+
+        [InverseProperty("IdUsuarioNavigation")]
+        public virtual ICollection<Venta> Venta { get; set; } = new List<Venta>();
+    }
+}
